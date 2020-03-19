00e2a9e548bfdc497688832a04c00be35ce92a43
neo-pule <malemelaneopule@gmail.com>
2020-03-19 18:30:47 +0200

    215449270-Assignment-1

--- 215449270.py
+++ 215449270.py
@@ -1,5 +1,5 @@
 #####################################
-#### EXERCISE 1 - 30 MARKS#######
+#### EXERCISE 1 30 MARKS#######
 #####################################
 
 # Time to review all the basic data types we learned! This should be a
@@ -24,8 +24,7 @@ print(s[9:])
 # 'cks'
 print(s[7:10])
 # Bonus: Use indexing to reverse the string
-reversedstring = '' .join(reversed(s))
-print(reversedstring)
+print(s[::-1])
 
 ###############
 ## Problem 2 - LISTS - 5 Marks##
@@ -33,12 +32,9 @@ print(reversedstring)
 
 # Given this nested list:
 l = [3,7,[5,[1,4,'hello']]]
-
 # Reassign "hello" to be "goodbye"
-l[2][1][2] = 'goodbye'
+l[2][1][2]= 'goodbye'
 print(l)
-
-print(l[2][1][2])
 
 ###############
 ## Problem 3 - DICTIONARIES - 6 Marks##
@@ -47,16 +43,12 @@ print(l[2][1][2])
 # Using keys and indexing, grab the 'hello' from the following dictionaries:
 
 d1 = {'simple_key':'hello'}
-
 print(d1['simple_key'])
 
 d2 = {'k1':{'k2':'hello'}}
-
 print(d2['k1']['k2'])
-
 d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
-
-print(d3['k1'][0]['nest_key'][1])
+print(d3['k1'][0]['nest_key'][1][0])
 
 ###############
 ## Problem 4 - SETS - 4 Marks##
@@ -64,11 +56,9 @@ print(d3['k1'][0]['nest_key'][1])
 
 # Use a set to find the unique values of the list below:
 mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
-unique_list = []
-for mylist in mylist:
-    if mylist not in unique_list:
-        unique_list.append(mylist)
-print(unique_list)
+
+p = set(mylist)
+print(p)
 
 ###############
 ## Problem 5  - FORMATTING - 5 Marks##
@@ -79,5 +69,4 @@ age = 45
 name = "Kyle"
 
 # Use print formatting to print the following string:
-"Hello my dog's name is Kyle and he looks 45 years old"
-print( "Hello my dog's name is {} and he looks year old {}" .format(name,age))
+print("Hello my dog's name is {} and he looks {} years old".format(name,age))
